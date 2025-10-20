d77c98f410d340094b4b0cf1734a4bece7b5c20b
Hadock <vasya.falyovskij@gmail.com>
2025-10-20 10:50:03 +0300

    1-2

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Привіт! Flask сервер працює 🚀"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
